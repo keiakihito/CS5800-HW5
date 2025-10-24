b9a2ab34eb1a95cc4fc7d92b619d96d8dd6ae38f
keiakihito <kkatsumi.git@gmail.com>
2025-10-24 16:20:58 -0700

    [Update] Implement StyledCharacter class and passed the unit test.

--- flyweight/src/main/java/flyweight/StyledCharacter.java
+++ flyweight/src/main/java/flyweight/StyledCharacter.java
@@ -1,4 +1,23 @@
 package flyweight;
 
 public class StyledCharacter {
+    private final char _character;
+    private final CharacterStyle _style;
+
+    public StyledCharacter(char character, CharacterStyle style) {
+        _character = character;
+        _style = style;
+    }
+
+    public char getCharacter() {
+        return _character;
+    }
+
+    public CharacterStyle getStyle() {
+        return _style;
+    }
+
+    public String describe() {
+        return _character + " -> " + _style.describe();
+    }
 }
