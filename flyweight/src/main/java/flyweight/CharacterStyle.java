5530baf18b8968f68c7f82e22b7fe358fa44d50d
keiakihito <kkatsumi.git@gmail.com>
2025-10-24 16:17:01 -0700

    [Update] Implement CharacterStyle class and passed the unit test.

--- flyweight/src/main/java/flyweight/CharacterStyle.java
+++ flyweight/src/main/java/flyweight/CharacterStyle.java
@@ -1,4 +1,34 @@
 package flyweight;
 
 public class CharacterStyle {
+    private final Font _font;
+    private final TextColor _color;
+    private final int _size;
+
+    public CharacterStyle(Font font, TextColor color, int size) {
+        _font = font;
+        _color = color;
+        _size = size;
+    }
+
+    public Font getFont() {
+        return _font;
+    }
+
+    public TextColor getColor() {
+        return _color;
+    }
+
+    public int getSize() {
+        return _size;
+    }
+
+    public String describe() {
+        return String.format("%s, %s, %dpt", capitalize(_font), capitalize(_color), _size);
+    }
+
+    private String capitalize(Enum<?> value) {
+        String lower = value.name().toLowerCase();
+        return Character.toUpperCase(lower.charAt(0)) + lower.substring(1);
+    }
 }
