58e07fde4ce16ec89089e6b97b4557924f713922
keiakihito <kkatsumi.git@gmail.com>
2025-10-28 07:32:04 -0700

    [Update] Implement Song and Songtest. Passed the unit test and got greenbar.

--- proxy/src/main/java/proxy/Song.java
+++ proxy/src/main/java/proxy/Song.java
@@ -1,4 +1,71 @@
 package proxy;
 
+import java.util.Objects;
+
 public class Song {
+    private final Integer _id;
+    private final String _title;
+    private final String _artist;
+    private final String _album;
+    private final int _duration;
+
+    public Song(Integer id, String title, String artist, String album, int duration) {
+        this._id = id;
+        this._title = title;
+        this._artist = artist;
+        this._album = album;
+        this._duration = duration;
+    }
+
+    public Integer getId() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public String getArtist() {
+        return _artist;
+    }
+
+    public String getAlbum() {
+        return _album;
+    }
+
+    public int getDuration() {
+        return _duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Song)) {
+            return false;
+        }
+        Song song = (Song) o;
+        return _duration == song._duration
+                && Objects.equals(_id, song._id)
+                && Objects.equals(_title, song._title)
+                && Objects.equals(_artist, song._artist)
+                && Objects.equals(_album, song._album);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, _title, _artist, _album, _duration);
+    }
+
+    @Override
+    public String toString() {
+        return "Song{"
+                + "_id=" + _id
+                + ", _title='" + _title + '\''
+                + ", _artist='" + _artist + '\''
+                + ", _album='" + _album + '\''
+                + ", _duration=" + _duration
+                + '}';
+    }
 }
--- proxy/src/test/java/proxy/SongTest.java
+++ proxy/src/test/java/proxy/SongTest.java
@@ -1,10 +1,31 @@
 package proxy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
 class SongTest {
+
     @Test
-    void placeholder() {
-        // TODO: Implement test cases for Song class
+    void storesProvidedMetadata() {
+        Song song = new Song(1, "Africa", "Toto", "Toto IV", 295);
+
+        assertEquals(1, song.getId());
+        assertEquals("Africa", song.getTitle());
+        assertEquals("Toto", song.getArtist());
+        assertEquals("Toto IV", song.getAlbum());
+        assertEquals(295, song.getDuration());
+    }
+
+    @Test
+    void equalityBasedOnAllFields() {
+        Song song1 = new Song(2, "Birdland", "Weather Report", "Heavy Weather", 204);
+        Song song2 = new Song(2, "Birdland", "Weather Report", "Heavy Weather", 204);
+        Song different = new Song(3, "Chicken", "Weather Report", "Live at Montreux", 187);
+
+        assertEquals(song1, song2);
+        assertEquals(song1.hashCode(), song2.hashCode());
+        assertNotEquals(song1, different);
     }
 }
